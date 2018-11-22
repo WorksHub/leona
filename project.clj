9491b6884fb556376ca4737b42c57bd6e80abef0
Antony Woods <antony@mastodonc.com>
2018-11-22 22:48:17 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leona "0.1.0-SNAPSHOT"
+(defproject leona "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
