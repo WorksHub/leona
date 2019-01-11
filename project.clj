7d663b0d046da307d7785cee4cfd723c2695bd05
Antony Woods <antony@mastodonc.com>
2019-01-11 15:00:02 +0000

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leona "0.1.1"
+(defproject leona "0.1.2-SNAPSHOT"
   :description "A pipeline for working with clojure.spec and GraphQL"
   :url "https://github.com/acron0/leona"
   :license {:name "Eclipse Public License"
