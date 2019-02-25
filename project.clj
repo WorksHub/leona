67aec8816cdf4cea0a95ef0d45a60ef52230f789
Daniel Janus <dj@danieljanus.pl>
2019-02-25 12:33:59 +0100

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject workshub/leona "0.1.3"
+(defproject workshub/leona "0.1.4-SNAPSHOT"
   :description "A pipeline for working with clojure.spec and GraphQL"
   :url "https://github.com/acron0/leona"
   :license {:name "Eclipse Public License"
