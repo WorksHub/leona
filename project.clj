5e2d89f343295a153a6f14546a640f4a0be6fd2f
Antony Woods <acron1@gmail.com>
2019-07-15 17:30:47 +0100

    Version 0.1.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject workshub/leona "0.1.15-SNAPSHOT"
+(defproject workshub/leona "0.1.15"
   :description "A pipeline for working with clojure.spec and GraphQL"
   :url "https://github.com/WorksHub/leona"
   :license {:name "Eclipse Public License"
